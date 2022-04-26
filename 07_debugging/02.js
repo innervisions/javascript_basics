b66e930706657459929d015509cec1607e20e888
Raymond Saade <raymond.saade@gmail.com>
2022-04-25 23:11:23 -0400

    completed debugging

---
+++ 07_debugging/02.js
@@ -0,0 +1,16 @@
+// #2 - Includes False
+function includesFalse(list) {
+  for (let i = 0; i < list.length; i++) {
+    let element = list[i];
+
+    if (element === false) {
+      return true;
+    }
+  }
+
+  return false;
+}
+// returns:
+console.log(includesFalse([8, null, 'abc', true, 'launch', 11, false]));       // true
+console.log(includesFalse(['programming', undefined, 37, 64, true, 'false'])); // false
+console.log(includesFalse([9422, 'lambda', true, 0, '54', null]));             // true
